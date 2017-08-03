ccce216a9b6a1b0e8e82ab774e19c6d491192d58
Michael Henry Pantaleon <me@iamkel.net>
2017-08-03 18:55:48 +0900

    just make it as an extension of nsobject.

--- SwiftAutoCoder/Classes/SwiftAutoCoder.swift
+++ SwiftAutoCoder/Classes/SwiftAutoCoder.swift
@@ -6,30 +6,24 @@
 //  Copyright © 2017 iamkel.net. All rights reserved.
 //
 
-protocol SwiftAutoCoder {
-    
-    func decodeX(_ aDecoder: NSCoder)
-    func encodeX(_ aCoder: NSCoder)
-}
 
-extension SwiftAutoCoder where Self:NSObject {
+extension NSObject {
     
-    var properties: [String] {
+    fileprivate var properties: [String] {
         return Mirror(reflecting: self).children.flatMap { $0.label }
     }
     
-    func decodeX(_ aDecoder: NSCoder) {
+    public func autoDecode(_ aDecoder: NSCoder) {
         properties.forEach { (keypath) in
             let keyValue = aDecoder.decodeObject(forKey: keypath)
             setValue(keyValue, forKey: keypath)
         }
     }
     
-    func encodeX(_ aCoder: NSCoder) {
+    public func autoEncode(with aCoder: NSCoder) {
         properties.forEach { (keypath) in
             let keyvalue = value(forKey: keypath)
             aCoder.encode(keyvalue, forKey: keypath)
         }
     }
-    
 }
